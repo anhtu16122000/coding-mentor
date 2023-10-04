76d7172674eb7feb29dfb1b78fc39ecb51dcec50
Uchiha Chaos <soc900k@gmail.com>
2023-10-04 11:34:15 +0700

    Update import

--- src/pages/info-course/feedbacks/detail/index.tsx
+++ src/pages/info-course/feedbacks/detail/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { MainLayout } from '~/common/index'
-import DetailFeedback from '../../../../common/pages/Info-Course/FeedBacks/Detail'
+import DetailFeedback from '~/common/pages/Info-Course/FeedBacks/Detail'
 
 function DetailFeedbackPage() {
 	return <DetailFeedback />
