21bb61035e9553c8df2a094a7aaa697e929cdd2e
Uchiha Chaos <soc900k@gmail.com>
2023-10-17 16:09:25 +0700

    Update htmlParser

--- src/common/components/HtmlParser/index.tsx
+++ src/common/components/HtmlParser/index.tsx
@@ -2,7 +2,15 @@ import ReactHtmlParser from 'react-html-parser'
 
 const htmlParser = (htmlString?: string) => {
 	if (!htmlString) return ''
-	return ReactHtmlParser(htmlString)
+
+	function removeContentEditable(text) {
+		// Sử dụng regex để loại bỏ các đoạn văn bản có contenteditable="true"
+		var regex = /contenteditable="true"/gi
+		var result = text.replace(regex, '')
+		return result
+	}
+
+	return ReactHtmlParser(removeContentEditable(htmlString))
 }
 
 export default htmlParser
