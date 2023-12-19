3587474d58a327c5ab331121faaa6f1f78a35aa9
ntq2712 <trongqui2712@gmail.com>
2023-12-19 16:14:35 +0700

    update rating

--- src/common/pages/PackagedExam/Detail/Ratings.tsx
+++ src/common/pages/PackagedExam/Detail/Ratings.tsx
@@ -171,25 +171,24 @@ function Ratings({ SkillsId }) {
 							</Col>
 						</Col>
 						{!!ranks[3] ? (
-							<Col span={24} className="flex items-center bg-tw-disable pt-[4px] pb-[4px] rounded-md">
-								<Col span={4}>
-									<div className="flex items-center justify-center bg-tw-blue rounded-md text-tw-white font-bold w-[48px] pt-[8px] pb-[8px]">
-										4
-									</div>
-								</Col>
-								<Col span={12}>
-									<div className="font-bold flex items-center">
-										<img
-											className="h-[40px] w-[40px] rounded-[50%] mr-[4px]"
-											src="https://play-lh.googleusercontent.com/2NJb9nukCV958oxsXuPQwF3hGcTTp21k9fIojW3Z5HfW42TsXXcdBDo_IVtanZDF0BmC"
-										/>
-										Nguyễn Thị Thanh Thảo
-									</div>
-								</Col>
-								<Col span={6}>
-									<div>2</div>
-								</Col>
-							</Col>
+							ranks.slice(3).map((_item) => (
+								<Col span={24} className="flex items-center bg-tw-disable pt-[4px] pb-[4px] rounded-md">
+									<Col span={4}>
+										<div className="flex items-center justify-center bg-tw-blue rounded-md text-tw-white font-bold w-[48px] pt-[8px] pb-[8px]">
+											{_item.Rank}
+										</div>
+									</Col>
+									<Col span={12}>
+										<div className="font-bold flex items-center">
+											<img className="h-[40px] w-[40px] rounded-[50%] mr-[4px]" src={_item.StudentThumbnail} />
+											{_item.StudentName}
+										</div>
+									</Col>
+									<Col span={6}>
+										<div>{_item.MyPoint}</div>
+									</Col>
+								</Col> 
+							))
 						) : (
 							<Col span={24} className="flex items-center bg-tw-disable pt-[4px] pb-[4px] rounded-md">
 								<Col span={4}>
